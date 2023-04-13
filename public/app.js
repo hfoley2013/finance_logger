4ed0db535bda8dbb3ba8a3b686f62ae48221fdb4
Harper Foley <harper.e.foley@gmail.com>
2023-04-12 21:42:01 -0700

    Renders Invoices and Payments to the DOM within a ul.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 // Holds all invoices
 let invoices = [];
@@ -13,6 +14,9 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// Instantiate ListTemplate
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -25,5 +29,5 @@ form.addEventListener('submit', (e) => {
         payments.push(doc);
     }
     docs.push(doc);
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
