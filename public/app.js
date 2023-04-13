434c6dee6a5c912a7e128ba7b87e82d6ae7ba456
Harper Foley <harper.e.foley@gmail.com>
2023-04-12 20:33:07 -0700

    Adds Invoice class.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,15 @@
 "use strict";
+// Classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}.`;
+    }
+}
 // Get form from DOM
 const form = document.querySelector('.new-item-form');
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+// Classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}.`;
+  }
+}
+
 // Get form from DOM
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
